da623d19a230f81fd43d21fa0416554cf25b8a57
RitaRamos <rita.mparada.ramos@gmail.com>
2019-11-10 23:06:52 +0000

    update training removing what is now on dataset file

--- src/train.py
+++ src/train.py
@@ -1,5 +1,7 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
+import logging
 import os
 import random
 from pickle import dump, load
@@ -10,6 +12,14 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
+from args_parser import get_args
+from dataset import get_dataset, get_vocab_info
+from generators.abstract_generator import PATH
+from generators.features_extracted.augmented_generator import (
+    FeaturesExtractedAugmentedGenerator, FineTunedAugmentedGenerator)
+from generators.features_extracted.simple_generator import \
+    FeaturesExtractedSimpleGenerator
+from generators.fine_tuned.simple_generator import FineTunedSimpleGenerator
 from models.abstract_model import BATCH_SIZE
 from models.simple_encoder_decoder import SimpleEncoderDecoderModel
 from models.simple_model import SimpleModel
@@ -20,13 +30,6 @@ from preprocess_data.simple_data.preprocess_data import (extract_features,
                                                          simple_generator)
 from preprocess_data.tokens import (END_TOKEN, START_TOKEN,
                                     convert_captions_to_Y, preprocess_tokens)
-
-from generators.features_extracted.simple_generator import FeaturesExtractedSimpleGenerator
-from generators.features_extracted.augmented_generator import FeaturesExtractedAugmentedGenerator
-from generators.fine_tuned.simple_generator import FineTunedSimpleGenerator
-from generators.features_extracted.augmented_generator import FineTunedAugmentedGenerator
-import logging
-import argparse
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['PYTHONHASHSEED'] = '0'
@@ -39,80 +42,32 @@ tf.random.set_seed(42)
 FEATURE_EXTRACTION = True
 AUGMENT_DATA = False
 
-PATH = "src/datasets/RSICD/"
-IMAGE_PATH = "/RSICD_images/"
-
-
-parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
-parser.add_argument(
-    '--file_name', help='name of file that was used to fill the all other arguments', default=None)
-# parser.add_argument('--fine_tuning',
-#                     help='set True to use feature extraction, otherwise fine-tuning', default=False, type=bool)
-# parser.add_argument(
-#     '--augment_data', help='set True to augment data', default=False)
-
-parser.add_argument(
-    '--model_class_str', help='class name of the model to train', default="SimpleModel")
-
-parser.add_argument('--augment_data', action='store_true', default=False,
-                    help='Set a switch to true')
-
-parser.add_argument('--fine_tuning', action='store_true', default=False,
-                    help='Set a switch to true')
-
-
-args = parser.parse_args()
-
-print(args.__dict__)
-
-
-def get_images_and_captions(dataset):
-    images_names = []
-    captions_of_tokens = []
-    for row in dataset["images"]:
-        image_name = row["filename"]
-        for caption in row["sentences"]:
-            tokens = [START_TOKEN] + caption["tokens"] + [END_TOKEN]
-
-            captions_of_tokens.append(tokens)
-            images_names.append(image_name)
-
-    images_names, captions_of_tokens = shuffle(
-        images_names, captions_of_tokens, random_state=42)
-    return images_names, captions_of_tokens
-
 
 # run as: PYTHONHASHSEED=0 python train.py
 if __name__ == "__main__":
     logging.basicConfig(
         format='%(levelname)s: %(message)s', level=logging.INFO)
 
+    args = get_args()
+    logging.info(args.__dict__)
+
     logging.info("Num GPUs Available: %s", len(
         tf.config.experimental.list_physical_devices('GPU')))
 
     logging.info("load and split dataset")
-    raw_dataset = pd.read_json(PATH+"dataset_rsicd.json")
-    raw_dataset = raw_dataset.sample(frac=1, random_state=42)
-    train, validation, test = np.split(
-        raw_dataset, [int(.8*len(raw_dataset)), int(.9*len(raw_dataset))])
+    raw_dataset = pd.read_json(PATH + "dataset_rsicd.json")
 
-    logging.info(
-        "transform dataset into respective images [[img_name]...] and captions [[token1,token2,...]...]")
-    train_images_names, train_captions_of_tokens = get_images_and_captions(
-        train)
-    val_images_names, val_captions_of_tokens = get_images_and_captions(
-        validation)
-    test_images_names, test_captions_of_tokens = get_images_and_captions(test)
+    vocab_info = get_vocab_info("src/datasets/RSICD/dataset/")
+    vocab_size, token_to_id, id_to_token, max_len = vocab_info[
+        "vocab_size"], vocab_info["token_to_id"], vocab_info["id_to_token"], vocab_info["max_len"]
 
-    vocab_size, token_to_id, id_to_token, max_len = preprocess_tokens(
-        train_captions_of_tokens
-    )  # preprocess should be done with trainset
+    train = get_dataset(
+        "src/datasets/RSICD/dataset/train.json")
+    train_images_names, train_captions_of_tokens = train["images_names"], train["captions_tokens"]
 
-    vocab_info = {}
-    vocab_info["vocab_size"] = vocab_size
-    vocab_info["token_to_id"] = token_to_id
-    vocab_info["id_to_token"] = id_to_token
-    vocab_info["max_len"] = max_len
+    val = get_dataset(
+        "src/datasets/RSICD/dataset/val.json")
+    val_images_names, val_captions_of_tokens = val["images_names"], val["captions_tokens"]
 
     logging.info(
         "captions tokens -> captions ids, since the NN doesnot read tokens but rather numbers")
@@ -120,8 +75,6 @@ if __name__ == "__main__":
         train_captions_of_tokens, max_len, token_to_id)
     val_input_captions, val_target_captions = convert_captions_to_Y(
         val_captions_of_tokens, max_len, token_to_id)
-    test_input_captions, test_target_captions = convert_captions_to_Y(
-        test_captions_of_tokens, max_len, token_to_id)
 
     logging.info(
         "images names -> images vectors (respective representantion of an image)")
@@ -191,14 +144,6 @@ if __name__ == "__main__":
     logging.info("create and run model")
     logging.info("qual é o tamanho do input %s",
                  generator.get_shape_of_input_image())
-    # model = SimpleModel(
-    #     str(args.__dict__),
-    #     vocab_size,
-    #     max_len,
-    #     generator.get_shape_of_input_image()
-    # )
-
-    #myvar = SimpleModel
 
     model_class = globals()[args.model_class_str]
 
@@ -208,8 +153,6 @@ if __name__ == "__main__":
         max_len,
         generator.get_shape_of_input_image()
     )
-
-    # SimpleEncoderDecoderModel
 
     model.create()
     model.summary()
@@ -233,30 +176,9 @@ if __name__ == "__main__":
     #     tf.expand_dims(
     #         img_tensor, axis=0), token_to_id, id_to_token))
 
-# Hoje
-# dar erro qd o modelo q tens de usar qd tas em feature extraction
-if feature:
-    class_name in []
 
-# perceber como é feito o Test!! (eu penso q é fazer load tal como aqui e depois testar)
-# 1 º tens q ter algo q faz save do dataset! (n em cenas -> train_images, train_captions...)
-Rscid:
-    raw{
-
-    }
-    dataset
-    train
-    val
-    test
-dataset.py
-
-# 2 º tens q ter vocab_load
-
-
-# Save token to id, max_len,len para os testes usarem!![de manha]
 # Tentar por teste e assim (a idea gerar de loading) -> save do modelo e se é fine-tuning ou não [até às 17:00]
 # por os modelos numa configuração (qual o modelo q vais usar...) [fazer os files]
-
 # por os modelos a correr o mesmo, fazer last check com todos!!
 # Por os valores dos modelos correctos (epochs, etc)
 # por os modelos a correr o mesmo , fazer last check com todos!!
