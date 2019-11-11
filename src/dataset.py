566a7277455a2333c8c5d91a4bfd0192a95e26c4
RitaRamos <rita.mparada.ramos@gmail.com>
2019-11-11 15:39:52 +0000

    update id_to_token from json.load being converted from string to in. Also refactor private method

--- src/dataset.py
+++ src/dataset.py
@@ -11,7 +11,7 @@ from preprocess_data.tokens import (END_TOKEN, START_TOKEN,
                                     convert_captions_to_Y, preprocess_tokens)
 
 
-def get_images_and_captions(dataset):
+def _get_images_and_captions(dataset):
     images_names = []
     captions_of_tokens = []
     for row in dataset["images"]:
@@ -27,7 +27,7 @@ def get_images_and_captions(dataset):
     return images_names, captions_of_tokens
 
 
-def dump_data_to_json(images_names, captions_tokens, file_dir, file_name):
+def _dump_data_to_json(images_names, captions_tokens, file_dir, file_name):
     dataset_dict = {
         "images_names": images_names,
         "captions_tokens": captions_tokens
@@ -37,7 +37,7 @@ def dump_data_to_json(images_names, captions_tokens, file_dir, file_name):
         json.dump(dataset_dict, f, indent=2)
 
 
-def dump_vocab_to_json(vocab_size, token_to_id, id_to_token, max_len, file_dir):
+def _dump_vocab_to_json(vocab_size, token_to_id, id_to_token, max_len, file_dir):
     vocab_info = {}
     vocab_info["vocab_size"] = vocab_size
     vocab_info["token_to_id"] = token_to_id
@@ -48,31 +48,33 @@ def dump_vocab_to_json(vocab_size, token_to_id, id_to_token, max_len, file_dir):
         json.dump(vocab_info, f, indent=2)
 
 
-def save_dataset(raw_dataset, file_dir):
+def _save_dataset(raw_dataset, file_dir):
+    # suffle and split dataset into train,val and test
     raw_dataset = raw_dataset.sample(frac=1, random_state=42)
     train, validation, test = np.split(
         raw_dataset, [int(.8*len(raw_dataset)), int(.9*len(raw_dataset))])
 
-    logging.info(
-        "transform dataset into respective images [[img_name]...] and captions [[token1,token2,...]...]")
-    train_images_names, train_captions_of_tokens = get_images_and_captions(
+    # "transform dataset into respective images [[img_name]...] and captions [[token1,token2,...]...]")
+    train_images_names, train_captions_of_tokens = _get_images_and_captions(
         train)
-    val_images_names, val_captions_of_tokens = get_images_and_captions(
+    val_images_names, val_captions_of_tokens = _get_images_and_captions(
         validation)
-    test_images_names, test_captions_of_tokens = get_images_and_captions(test)
+    test_images_names, test_captions_of_tokens = _get_images_and_captions(test)
 
     vocab_size, token_to_id, id_to_token, max_len = preprocess_tokens(
         train_captions_of_tokens
     )  # preprocess should be done with trainset
 
-    dump_vocab_to_json(vocab_size, token_to_id, id_to_token, max_len, file_dir)
+    # save vocab and datasets
+    _dump_vocab_to_json(vocab_size, token_to_id,
+                        id_to_token, max_len, file_dir)
 
-    dump_data_to_json(train_images_names, train_captions_of_tokens,
-                      file_dir, "train.json")
-    dump_data_to_json(val_images_names, val_captions_of_tokens,
-                      file_dir, "val.json")
-    dump_data_to_json(test_images_names, test_captions_of_tokens,
-                      file_dir,  "test.json")
+    _dump_data_to_json(train_images_names, train_captions_of_tokens,
+                       file_dir, "train.json")
+    _dump_data_to_json(val_images_names, val_captions_of_tokens,
+                       file_dir, "val.json")
+    _dump_data_to_json(test_images_names, test_captions_of_tokens,
+                       file_dir,  "test.json")
 
 
 def get_dataset(file_path):
@@ -84,10 +86,21 @@ def get_dataset(file_path):
 def get_vocab_info(file_dir):
     with open(file_dir+"vocab_info.json") as json_file:
         vocab_info = json.load(json_file)
+
+    # given it was loaded from a json, the dict id_to_token has keys as strings instead of int, as supposed. To fix:
+    vocab_info["id_to_token"] = {
+        int(k): v for k, v in vocab_info["id_to_token"].items()}
+
     return vocab_info
 
 
 if __name__ == "__main__":
-    # save train,test and val of RSCID
+    logging.basicConfig(
+        format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    logging.info("start to save datasets and vocab of of RSCID")
+
     raw_dataset = pd.read_json(PATH + "dataset_rsicd.json")
-    save_dataset(raw_dataset, "src/datasets/RSICD/dataset/")
+    _save_dataset(raw_dataset, "src/datasets/RSICD/dataset/")
+
+    logging.info("saved datasets and vocab")
