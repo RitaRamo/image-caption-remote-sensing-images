5f92478ad24adf8eb49baaf97d216ebfb14328b4
RitaRamos <rita.mparada.ramos@gmail.com>
2019-12-16 12:00:09 +0000

    just to check

--- src/evaluator/evaluate.py
+++ src/evaluator/evaluate.py
@@ -15,7 +15,7 @@ class Evaluator():
         predicted = []
         # nlgeval = NLGEval()  # loads the models
         # ifi ==3 ver se dá
-        i = 0
+
         for img_name in test_dataset.keys():
 
             img_tensor = self.generator.get_image(img_name)
@@ -28,26 +28,25 @@ class Evaluator():
             # scores = self.compare_results(
             #     references_captions_of_image, text_generated)
             predicted.append(text_generated)
-            i += 1
-            if i == 5:
-                break
+            # i += 1
+            # if i == 5:
+            #     break
 
-        references_captions = list(test_dataset.values())[:i]
-
-        print("len ref", len(references_captions))
-        print("len caption", len(predicted))
-        print("equal ", len(references_captions) == len(predicted))
+        references_captions = list(test_dataset.values())
 
         scores = self.compare_results(
             references_captions, predicted)
         # so funca se tivr + q um elemento, senão usar o outro para comparar!
-        # TODO: fazer score individualmnt, guardando as categorias do maior score e as do pior
 
         return scores
 
     def compare_results(self, references_captions, predicted_captions):
         print("this are reds", references_captions)
         print("this are predicted_caption", predicted_captions)
+
+        print("len ref", len(references_captions))
+        print("len caption", len(predicted))
+        print("equal ", len(references_captions) == len(predicted))
 
         nlgeval = NLGEval()  # loads the models
         metrics_dict = nlgeval.compute_metrics(
