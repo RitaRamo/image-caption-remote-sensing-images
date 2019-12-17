89fecf640717699eab8ab6f103ebc01fa0ffa05b
RitaRamos <rita.mparada.ramos@gmail.com>
2019-12-17 10:55:15 +0000

    update

--- experiments/conf_files/run_all_models.sh
+++ experiments/conf_files/run_all_models.sh
@@ -1,2 +1,2 @@
-PYTHONHASHSEED=0 python3 src/train.py @experiments/conf_files/FE_S_SM.txt
-PYTHONHASHSEED=0 python3 src/train.py @experiments/conf_files/FE_A_SM.txt
+PYTHONHASHSEED=0 python3 src/train.py @experiments/conf_files/FE_S_BM.txt
+PYTHONHASHSEED=0 python3 src/train.py @experiments/conf_files/FE_A_BM.txt
