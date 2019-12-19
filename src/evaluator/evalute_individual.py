721b591febf44906be6470a1f9de3e2a4cc36d3d
RitaRamos <rita.mparada.ramos@gmail.com>
2019-12-19 09:28:07 +0000

    fix bug

--- src/evaluator/evalute_individual.py
+++ src/evaluator/evalute_individual.py
@@ -15,9 +15,10 @@ class EvaluatorIndividualMetrics():
     def evaluate(self, test_dataset):
         predicted = []
         metrics = {}
-        # nlgeval = NLGEval()  # loads the models
+        nlgeval = NLGEval()  # loads the models
         # ifi ==3 ver se dá
         n_comparations = 0
+
         for img_name, references in test_dataset.items():
 
             img_tensor = self.generator.get_image(img_name)
@@ -30,22 +31,23 @@ class EvaluatorIndividualMetrics():
             # scores = self.compare_results(
             #     references_captions_of_image, text_generated)
             predicted.append(text_generated)
-            scores = self.compare_results(references, text_generated)
+            scores = self.compare_results(nlgeval, references, text_generated)
             for metric, score in scores.items():
-                metrics[metric] += score
+                if metric not in metrics:
+                    metrics[metric] = score
+                else:
+                    metrics[metric] += score
             n_comparations += 1
-            break
 
         avg_metrics = {metric: total_score /
                        n_comparations for metric, total_score in metrics.items()}
 
         return avg_metrics
 
-    def compare_results(self, references_captions, predicted_captions):
+    def compare_results(self, nlgeval, references_captions, predicted_captions):
         print("ref", references_captions)
         print("caption", predicted_captions)
 
-        nlgeval = NLGEval()  # loads the models
         metrics_dict = nlgeval.compute_individual_metrics(
             references_captions, predicted_captions)
         print("this are dic metrics", metrics_dict)
